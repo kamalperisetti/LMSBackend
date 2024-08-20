19c4ef872963507f3f4405a5102eb5958342a089
kamalperisetti <perisettikamal@gmail.com>
2024-08-20 15:20:59 +0530

    changed url

--- src/main/java/com/project/learningmanagemtsystem/config/WebConfig.java
+++ src/main/java/com/project/learningmanagemtsystem/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:5173")
+        registry.addMapping("/**").allowedOrigins("https://lms-frontend-beige-tau.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
